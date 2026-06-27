b21391f5829d9eb177606c7497278182cce337cf
OmarLucky102 <omarabdelaty101@gmail.com>
2026-06-27 16:33:44 +0300

    create the DTO

---
+++ src/profiles/dto/create-profile.dto.ts
@@ -0,0 +1,5 @@
+//classe stick around during the run time
+export class CreateProfileDto {
+  name: string;
+  description: string;
+}
--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 //decorator higher order function
 @Controller('profiles')
@@ -9,8 +10,17 @@ export class ProfilesController {
     return [{ location }];
   }
   //GET/profiles/:id
+  //Param is to grape the id
   @Get(':id')
   findOne(@Param('id') id: string) {
     return { id };
+  }
+  //Create a data transfare object DTO what the data should look like
+  @Post()
+  create(@Body() CreateProfileDto: CreateProfileDto) {
+    return {
+      name: CreateProfileDto.name,
+      description: CreateProfileDto.description,
+    };
   }
 }
